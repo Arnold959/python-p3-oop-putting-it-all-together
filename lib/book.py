0a8630ccbdc0a10228d02460fa3f0d57820df034
arnold <musaliokello@gmail.com>
2023-05-21 21:15:34 +0300

    Completed assignment

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python3
 
 class Book:
+    def __init__(self, title, author, publication_year):
+        self.title = title
+        self.author = author
+        self.publication_year = publication_year
+
+    def get_title(self):
+        return self.title
+
+    def get_author(self):
+        return self.author
+
+    def get_publication_year(self):
+        return self.publication_year
     pass
     
         
